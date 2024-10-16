a714e04f31fa948ec9dbb3e3e3d443a6d118ea89
Mehdiii91 <mehdi.triaa2901@gmail.com>
2024-10-16 16:20:45 +0200

    test_sample.py uplaod

--- machine learning/cours/test/test_sample.py
+++ machine learning/cours/test/test_sample.py
@@ -2,6 +2,18 @@
 def inc(x):
     return x + 1
 
+def palindrome(s):
+    """
+    ceci est une fonction pour detecter les palaindromes !!!
+    params:
+    s:string
+    """
+    if s==s[::-1]:
+        return True
+    return False
 
 def test_answer():
     assert inc(3) == 4
+
+def test_palindrome():
+    assert palindrome("kayak")==True
